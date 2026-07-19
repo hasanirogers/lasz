bc7e6f16c9058d58d3c7b5ee691c6e3ec51dfbc4
Hasani Rogers <contact@deificarts.com>
2026-07-19 12:55:32 -0700

    more build adjuments

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
   server: {
     port: 4321
   },
   adapter: node({
-    mode: 'standalone'
+    mode: 'middleware'
   }),
 });

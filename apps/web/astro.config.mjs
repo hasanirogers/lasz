445743e1972d723ef83a5e823a486673ebc1e54a
Hasani Rogers <contact@deificarts.com>
2026-07-20 05:52:06 -0700

    more build adjuments

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+// import { defineConfig } from 'astro/config';
+// import node from '@astrojs/node';
 
-export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
-});
+// export default defineConfig({
+//   output: 'server',
+//   adapter: node({
+//     mode: 'standalone'
+//   }),
+// });

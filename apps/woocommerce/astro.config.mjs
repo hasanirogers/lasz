8999f98cdf0f3ed36a4104548624bd650be72aa6
Hasani Rogers <contact@deificarts.com>
2026-08-07 17:15:07 -0700

    hmmmm

--- apps/woocommerce/astro.config.mjs
+++ apps/woocommerce/astro.config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
     // Inlines all CSS files smaller than 15kb directly into the HTML
     inlineStylesheets: 'always',
   },
+  output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
